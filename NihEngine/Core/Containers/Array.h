e3e251ad99d3855de7741026e727ec76fddb57a2
Nihiss-dev <alexis.feldmar@gmail.com>
2023-04-23 21:57:47 +0200

    Add Array container

--- NihEngine/Core/Containers/Array.h
+++ NihEngine/Core/Containers/Array.h
@@ -1,6 +1,42 @@
 #pragma once
 
 #include <array>
+#include "System/Assert.h"
 
 template <typename T, size_t size>
-using Array = std::array<T, size>;
+class Array
+{
+public:
+	[[nodiscard]] constexpr const T& At(size_t pos) const
+	{
+		NIH_ASSERT(pos < size);
+		return m_Data[pos];
+	}
+	[[nodiscard]] constexpr T& At(size_t pos)
+	{
+		NIH_ASSERT(pos < size);
+		return m_Data[pos];
+	}
+
+	[[nodiscard]] constexpr size_t GetSize() const { return size; };
+	[[nodiscard]] constexpr const T& Front() const { return m_Data[0]; }
+	[[nodiscard]] constexpr T& Front() { return m_Data[0]; }
+	[[nodiscard]] constexpr const T& Back() const { return m_Data[size - 1]; }
+	[[nodiscard]] constexpr T& Back() { return m_Data[size - 1]; }
+	[[nodiscard]] constexpr const T& operator[](size_t pos) const
+	{
+		NIH_ASSERT(pos < size);
+		return m_Data[pos];
+	}
+	[[nodiscard]] constexpr T& operator[](size_t pos)
+	{
+		NIH_ASSERT(pos < size);
+		return m_Data[pos];
+	}
+
+	T m_Data[size];
+};
+
+// Deducing guides that will let us write Array array{0,1,2,3} without specifying type or size
+template<class Type, class... Size>
+Array(Type, Size...) -> Array<Type, 1 + sizeof...(Size)>;
--- Tests/Containers/TestArray.cpp
+++ Tests/Containers/TestArray.cpp
@@ -3,9 +3,43 @@
 
 namespace Containers
 {
+	TEST(Array, Construct)
+	{
+		// These are here to make sure we will always be able to compile
+		{
+			Array<int, 4> array{ 0,1,2,3 };
+		}
+		{
+			Array array{ 0,1,2,3 };
+		}
+	}
 	TEST(Array, GetSize)
 	{
 		Array<int, 4> array{ 0,1,2,3 };
-		EXPECT_TRUE(array.size() == 4);
+		EXPECT_TRUE(array.GetSize() == 4);
+	}
+
+	TEST(Array, At)
+	{
+		Array<int, 4> array{ 0,1,2,3 };
+		EXPECT_TRUE(array.At(2) == 2);
+	}
+
+	TEST(Array, Front)
+	{
+		Array<int, 4> array{ 3,2,1,0 };
+		EXPECT_TRUE(array.Front() == 3);
+	}
+
+	TEST(Array, Back)
+	{
+		Array<int, 4> array{ 0,1,2,3 };
+		EXPECT_TRUE(array.Back() == 3);
+	}
+
+	TEST(Array, AtOperator)
+	{
+		Array<int, 4> array{ 0,1,2,3 };
+		EXPECT_TRUE(array[2] == 2);
 	}
 }
