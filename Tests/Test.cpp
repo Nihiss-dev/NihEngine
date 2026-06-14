c2f09da9e71e2a877d42c8567ffee8729a6b2c8d
Nihiss-dev <alexis.feldmar@gmail.com>
2026-06-14 16:27:31 +0200

    Fix Unit tests crashing on startup

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -4,3 +4,9 @@ TEST(ABC, TEST1)
 {
 	EXPECT_EQ(true, true);
 }
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
--- generate.bat
+++ generate.bat
@@ -10,8 +10,10 @@ if exist %BUILD_DIR% (
     rd /s /q %BUILD_DIR%
 )
 
+set GTEST_ARGS=-Dgtest_force_shared_crt=ON -DBUILD_SHARED_LIBS=OFF
+
 echo Generating solution...
-cmake -S . -B %BUILD_DIR% -G "%GENERATOR%" -A %ARCH%
+cmake -S . -B %BUILD_DIR% -G "%GENERATOR%" -A %ARCH% %GTEST_ARGS%
 
 if %ERRORLEVEL% neq 0 (
     echo.
